394575ee4d850d917b45e9c947c994a6c677375c
muhamad adli <adli01@gmail.com>
2025-02-19 23:02:22 +0700

    pjbl

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -13,25 +13,27 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+    <!-- aos -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 </head>
 
 <body>
     <!-- navbar -->
     <nav class="navbar">
         <div class="navbar-main">
-            <img src="img/smk2.png" alt="" width="50px" height="50px">
+            <img src="img/smk2.png" alt="sekolah" width="50px" height="50px">
             <p class="logo">ekstrakurikuler SMKN 2 Kota Bekasi</p>
         </div>
         <div class="navbar-nav">
             <ul>
                 <li><a href="#home">home</a></li>
                 <li><a href="#about">about</a></li>
-                <li><a href="#">Gallery</a></li>
+                <li><a href="#gallery">Gallery</a></li>
                 <li><a href="auth">login</a></li>
             </ul>
         </div>
         <div class="navbar-extra">
-            <a href="#" id="menu"><i class="bi bi-list"></i></a>
+            <a id="menu"><i class="bi bi-list"></i></a>
         </div>
     </nav>
     <!-- close navbar -->
@@ -66,24 +68,123 @@
         </div>
     </section>
 
+    <!-- section about -->
     <section class="about" id="about">
         <h2>about</h2>
-        <div class="container-about">
+        <div class="container-about" data-aos="fade-up" data-aos-offset="200" data-aos-delay="50"
+            data-aos-duration="1000" data-aos-easing="ease-in-out">
             <div class="about-img">
-                <img src="" alt="" width="100px" height="100px">
+                <img src="img/auth.jpg" alt="">
             </div>
             <div class="about-main">
                 <h4>ekstrakurikuler</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur magni, vel ut libero ea beatae
                     esse
                     temporibus adipisci saepe dignissimos cum aliquam vitae quaerat? Sunt repudiandae soluta suscipit
-                    adipisci nostrum.</p>
-                <button class="btn-learn">learn more</button>
+                    adipisci nostrum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi consequuntur libero
+                    repudiandae quae impedit aliquid pariatur quod nobis atque error, repellendus incidunt id officiis
+                    voluptatibus consectetur, vel iusto minima facilis. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Velit cupiditate optio voluptas alias, aut minima dolor ipsam</p>
+                <button class="btn-learn">learn more <i class="bi bi-arrow-right"></i></button>
             </div>
         </div>
     </section>
 
+    <!-- more about -->
+
+
+    <!-- section gallery -->
+    <section class="gallery" id="gallery">
+        <h2>gallery</h2>
+        <div class="gallery-container" data-aos="fade-up" data-aos-offset="200" data-aos-delay="50"
+            data-aos-duration="1000" data-aos-easing="ease-in-out">
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+            <div class="gallery-item">
+                <img src="img/auth.jpg" alt="gallery">
+                <div class="info">Lorem ipsum</div>
+                <hr>
+                <div class="icon">
+                    <p>100</p>
+                    <i class="bi bi-people-fill"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer-container">
+            <div class="footer-info">
+                <p>Copyright &copy; 2025 XII RPL 3 KELOMPOK 6.</p>
+                <p>All rights reserved</p>
+                <div class="social-icons">
+                    <a href="https://www.instagram.com/softwethre19/" target="_blank"><i
+                            class="bi bi-instagram"></i></a>
+                    <a href="https://www.tiktok.com/@19engineers3" target="_blank"><i class="bi bi-tiktok"></i></a>
+                    <a href="https://www.youtube.com/@rplsmkn2kotabekasi246"><i class="bi bi-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.297567622083!2d106.9895416750389!3d-6.355513493634445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698c22161d4051%3A0x7a0a35b288779341!2sSMKN%202%20Kota%20Bekasi!5e0!3m2!1sid!2sid!4v1739977806573!5m2!1sid!2sid"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgradeid" width="100%" height="200" style="border:0;"
+                    allowfullscreen="" loading="lazy">
+                </iframe>
+            </div>
+        </div>
+    </footer>
+
+    <!-- footer -->
     <script src="js/home.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>

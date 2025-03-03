6e77ebf9699d49e2dd148c3c479c7f2f875f61d4
muhamad adli <muhmdadli01@gmail.com>
2025-03-03 23:14:01 +0700

    pejebeel

---
+++ database/migrations/2025_03_03_160737_create_gallery_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gallery', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_eskul');
+            $table->string('foto');
+            $table->timestamps();
+
+            $table->foreign('id_eskul')->references('id_eskul')->on('eskul')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gallery');
+    }
+};

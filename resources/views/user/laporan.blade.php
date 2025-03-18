edb68b0dd5ed7deef1ade0e7c46118dfff86d700
muhamad adli <muhmdadli01@gmail.com>
2025-03-18 23:28:32 +0700

    pejebel

--- resources/views/user/laporan.blade.php
+++ resources/views/user/laporan.blade.php
@@ -35,13 +35,76 @@
                 <!-- Navbar -->
                 @include('partials.navbar')
                 <!-- end navbar -->
-            </div>
-        </div>
 
-        <script src="{{ asset('js/dashboard.js') }}"></script>
+                <!-- main content -->
+                <div class="grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6 p-6">
+                    <div class="flex flex-col md:flex-row justify-between items-center mb-6 space-y-4 md:space-y-0">
+                        <h2 class="text-2xl font-bold">
+                            Members Management
+                        </h2>
+                        <div class="flex flex-row items-center space-x-4 w-full md:w-auto">
+                            <div class="relative w-full md:w-60">
+                                <input
+                                    class="w-full p-2 pl-10 border rounded-lg bg-gray-100 text-gray-600 shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
+                                    placeholder="Search" type="text" />
+                                <i class="fas fa-search absolute left-3 top-3 text-gray-500">
+                                </i>
+                            </div>
+                            <button class="bg-blue-500 text-white px-4 py-2 rounded w-full md:w-auto">
+                                + Add Member
+                            </button>
+                        </div>
+                    </div>
+                    <div class="bg-white shadow-md rounded-lg p-4 ">
+                        <table class="min-w-full bg-white">
+                            <thead>
+                                <tr class="text-black">
+                                    <th class="py-2 px-4 border-b text-sm">
+                                        NO
+                                    </th>
+                                    <th class="py-2 px-4 border-b text-sm">
+                                        NAME
+                                    </th>
+                                    <th class="py-2 px-4 border-b text-sm">
+                                        CLASS
+                                    </th>
+                                    <th class="py-2 px-4 border-b text-sm">
+                                        MAJOR
+                                    </th>
+                                    <th class="py-2 px-4 border-b text-sm">
+                                        ACTIONS
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                <tr>
+                                    <td class="py-2 px-4 ">
+                                        1
+                                    </td>
+                                    <td class="py-2 px-4 ">
+                                        John Doe
+                                    </td>
+                                    <td class="py-2 px-4 ">
+                                        10
+                                    </td>
+                                    <td class="py-2 px-4 ">
+                                        RPL
+                                    </td>
+
+                                    <td class="py-2 px-4 ">
+                                        <i class="fas fa-edit text-gray-600 hover:text-gray-800 cursor-pointer mr-2">
+                                        </i>
+                                        <i class="fas fa-trash text-gray-600 hover:text-gray-800 cursor-pointer">
+                                        </i>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <script src="{{ asset('js/dashboard.js') }}"></script>
     </body>
 
     </html>
-</body>
-
-</html>
